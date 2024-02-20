050339e4bfb09e44e9b604e2229a41698c8e8a72
Oxidiu <oxidiu777@gmail.com>
2024-02-20 22:12:23 +0200

    server

--- backend/server.js
+++ backend/server.js
@@ -35,7 +35,7 @@ const __dirname = path.resolve();
 
 if (process.env.NODE_ENV === 'production') {
     // set static folder
-    app.use(express.static(path.join(__dirname, '../frontend/build')));
+    app.use(express.static(path.join(__dirname, '../frontend/dist')));
 
     // any route that is not api will be redirected to  the home page
     app.get('*', (req, res) => res.sendFile(path.resolve(__dirname, 'frontend, build,  index.html')));
