c61340c788d8891a67fd87f03074b1d6fc37d2d3
Oxidiu <oxidiu777@gmail.com>
2024-02-20 19:15:47 +0200

    production settings

--- backend/server.js
+++ backend/server.js
@@ -30,6 +30,17 @@ app.get('/', (req, res) => {
 app.use('/api/products', productRoutes)
 app.use('/api/users', userRoutes)
 
+if (process.env.NODE_ENV === 'production') {
+    // set static folder
+    app.use(express.static(path.join(__dirname, '../frontend/build')));
+
+    // any route that is not api will be redirected to  the home page
+    app.get('*', (req, res) => res.sendFile(path.resolve(__dirname, 'frontend, build,  index.html')));
+} else {
+    app.get('/', (req, res) => {
+        res.send('API is running...');
+    })
+}
 app.use(notFound);
 app.use(errorHandler);
 
